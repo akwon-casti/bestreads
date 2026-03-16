c6c3e6408c80f79d4e5b338b2caf6e54c6375ee1
akwon-casti <akwon@castilleja.org>
2026-03-16 13:50:37 -0700

    Initial Commit

---
+++ bestreads/bestreadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  bestreadsApp.swift
+//  bestreads
+//
+//  Created by Angela Kwon on 3/16/26.
+//
+
+import SwiftUI
+
+@main
+struct bestreadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ bestreadsTests/bestreadsTests.swift
@@ -0,0 +1,17 @@
+//
+//  bestreadsTests.swift
+//  bestreadsTests
+//
+//  Created by Angela Kwon on 3/16/26.
+//
+
+import Testing
+@testable import bestreads
+
+struct bestreadsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
